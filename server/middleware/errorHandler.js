51b9b4e48afcb8ca1c185ea2228fe3adfe71907b
Anshuman <anshumansr26@gmail.com>
2024-11-08 15:22:37 +0530

    trying changes

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next)=>{
     const statusCode = res.statusCode? res.statusCode:500;
     switch(statusCode){
         case constants.VALIDATION_ERROR: res.json({
-            title : "validation failed",
+            title : "validation failed...",
             message : err.message , 
             stackTrace : err.stack,
         });
